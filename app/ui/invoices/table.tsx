3054ce3f715ac76b508f396367230a47aced4e72
architectureYK <mahmedyk789@gmail.com>
2023-11-18 22:28:54 +0500

    fixing Image

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -1,8 +1,9 @@
-import Image from 'next/image';
+
 import { UpdateInvoice, DeleteInvoice } from '@/app/ui/invoices/buttons';
 import InvoiceStatus from '@/app/ui/invoices/status';
 import { formatDateToLocal, formatCurrency } from '@/app/lib/utils';
 import { fetchFilteredInvoices } from '@/app/lib/data';
+import Image from 'next/image';
 
 export default async function InvoicesTable({
   query,
@@ -31,6 +32,7 @@ export default async function InvoicesTable({
                         className="mr-2 rounded-full"
                         width={28}
                         height={28}
+                        alt='pic'
                       />
                       <p>{invoice.name}</p>
                     </div>
@@ -89,6 +91,7 @@ export default async function InvoicesTable({
                         className="rounded-full"
                         width={28}
                         height={28}
+                        alt='pic'
                       />
                       <p>{invoice.name}</p>
                     </div>
